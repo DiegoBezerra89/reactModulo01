06844b0b7c00b6c2f33bc5fbaa373b73ffd7831d
Diego Bezerra <diegobmartins21@gmail.com>
2019-09-24 17:49:41 -0300

    Funcoes puras, key props, explicação de props, criação de novos componentes ES6

--- src/app.js
+++ src/app.js
@@ -1,16 +1,37 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './title'
+import Square from './square'
+/*
+    há algumas mudanças em jsx e html
+    class se escreve className
+    <label for ... se escreve <label htmlFor
+*/
 
-const App = React.createClass({
-    render: () => {
+//Modo com classes Es6
+class App extends Component {
+    render () {
         return (
-            <div>
-                <Title nome="Diego Bezerra" />
-            </div>
+            <div className='container'>
+                {['red', 'green', 'blue'].map((square) => (
+                    <Square key = {square} color = {square} /> //cada key serve para identificar cada elemento
+                ))}
+            </div>    
         )
     }
-})
+}
+
+//Modo comum ES5
+// const App = React.createClass({
+//     render: function() {
+//         return (
+//             <div className='container'>
+//                 <Title name='Diego' lastName='Bezerra'/>
+//             </div>
+//         )
+//     }
+// })
 
 export default App
+
--- src/title.js
+++ src/title.js
@@ -2,12 +2,81 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-    render: function () {
-        return (
-            <h1>Olá {this.props.nome}, pica de ouro </h1>
-        )
-    }
-})
+//utilizando funções puras
+const Title = (props) => ( //há de se passar as props como parametro
+    <h1>Olá {`${props.name} ${props.lastName} testando`}</h1>
+)
+
+Title.defaultProps = {
+    name: 'Desconhecido',
+    lastName: 'Sem sobrenome'
+}
+
+//Modelo Antigo
+// const Title = React.createClass({
+//     //estas propriedades serão padrão caso as props não sejam passadas
+//     getDefaultProps: function() {
+//         return {
+//             name: 'Desconhecido'
+//         }
+//     },
+
+//     render: function () {
+//         return (
+//             <h1>Olá {this.props.name}</h1>
+//         )
+//     }
+// })
 
 export default Title
+
+
+
+// ================ ANOTAÇÕES ===================
+
+
+/*
+    //estas propriedades serão padrão caso as props não sejam passadas
+    getDefaultProps: function() {
+        return {
+            name: 'Desconhecido'
+        }
+    }
+
+    /PASSANDO VIA PROPS OUTROS TIPOS DE DADOS
+    <Title name='Diego Bezerra' age='0'(ou via string) ou age={0} entre chaves {}, qualquer elemento js entre chaves é aceito
+    arrays são convertidos como string [1,2,3] = 1,2,3
+    objetos = {{ name: first: 'Diego', lastName: 'bezerra'}}
+
+
+    // ###FUNÇÕES PURAS
+    function pure(a,b) {
+        return a + b
+    }
+    esse tipo de função não altera o resultado de nada externo a ela, e se ela for chamada varias vezes o resultados é o mesmo
+
+    // ###FUNÇÕES IMPURAS
+    obj {
+        a: 1, 
+        b: 2
+    }
+    function impureFunction(a,b) {
+        obj.a = a + b
+        return a + b
+    }
+    esse tipo de função altera o valor de algo externo a ela mesma
+
+    // ###FUNÇÕES IMPURAS
+    function impureFunction2 ( a, b ) {
+        return Math.random( a + b )
+    }
+    retornará sempre um valor diferente a cada chamada
+
+    >>>>>>>>>> no react o método Render, deve ser puro, ele não deve modificar o componente
+
+
+*/
+
+
+
+
