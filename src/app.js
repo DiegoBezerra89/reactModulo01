da048bd9eb29d9cc7d79975a1a9320624a90845b
unknown <Diego Bezerra>
2019-09-24 00:33:33 -0300

    Modulo 01 arquivos de base

---
+++ src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import Title from './title'
+
+const App = React.createClass({
+    render: () => {
+        return (
+            <div>
+                <Title nome="Diego Bezerra" />
+            </div>
+        )
+    }
+})
+
+export default App
